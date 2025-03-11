e3b3c19f0ad08f29f077bb4a4e0707ca2e65ca45
Muhammed Shan <muhammedshan@Muhammeds-MacBook-Air-2.local>
2025-03-11 15:39:57 +0530

    db changed and ready

--- fresh2home/settings.py
+++ fresh2home/settings.py
@@ -33,7 +33,7 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["fresh2home.vercel.app", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = []
 
 # Application definition
 
